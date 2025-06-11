acaa3b5717ce411154c5c135840bdc464983670a
Muhammad Afan Aljafar <muhammadafanaljafar@gmail.com>
2025-06-11 11:47:19 +0700

    trying to deploy

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,11 @@
 import "./Footer.css";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <p>© 2035 by Afan Aljafar. Powered and secured by ynoir space</p>
+    </div>
+  );
 };
 Footer.displayName = "Footer";
 
