7129d39a919617cad53101be8a8e4e4b56f3c150
Muhammad Afan Aljafar <muhammadafanaljafar@gmail.com>
2025-06-21 13:47:15 +0700

    update pages miniproject

--- src/components/temperatureConverter/TemperatureConverter.js
+++ src/components/temperatureConverter/TemperatureConverter.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TemperatureConverter.css";
 
 const TemperatureConverter = () => {
+  const [celsius, setCelsius] = useState(0);
+  const fahrenheit = (celsius * 9) / 5 + 32;
+
   return (
-    <div>
+    <div className="wrap-converter">
       <h2 className="title-temperature-converter">TemperatureConverter</h2>
-      <label>Temperature 1</label>
-      <input type="number" />
-      <label>Temperature 2</label>
-      <input type="number" />
+      <div className="container-converter">
+        <div className="converter-card">
+          <select className="dropdown full-width">
+            <option>Temperature</option>
+            <option>Length</option>
+            <option>Volume</option>
+          </select>
+        </div>
+      </div>
     </div>
   );
 };
