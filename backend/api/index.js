4d069af2763220a570be5de24466dd5675da7750
Muhammad Afan Aljafar <muhammadafanaljafar@gmail.com>
2025-07-22 15:26:56 +0700

    update

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,7 +1,7 @@
 const app = require("../src/index");
 const serverless = require("serverless-http");
 
-// module.exports = serverless(app);
-module.exports = (req, res) => {
-  app(req, res);
-};
+module.exports = serverless(app);
+// module.exports = (req, res) => {
+//   app(req, res);
+// };
