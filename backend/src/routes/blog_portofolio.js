9b0b1e96d6a095839e71bde391f38062b283a25a
afan <muhammadafanaljafar@gmail.com>
2025-07-22 14:02:06 +0700

    update

--- backend/src/routes/blog_portofolio.js
+++ backend/src/routes/blog_portofolio.js
@@ -3,6 +3,10 @@ const blogPortofolioController = require("../controller/blog_portofolio");
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.json({ message: "✅ Visitor route works from Vercel!" });
+});
+
 router.post("/create-testimonial", blogPortofolioController.createTestimonial);
 
 router.get("/display-testimonial", blogPortofolioController.displayTestimonial);
