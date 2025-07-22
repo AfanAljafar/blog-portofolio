bc3f45598a4d307218209960af6aea94dc6f3364
Muhammad Afan Aljafar <muhammadafanaljafar@gmail.com>
2025-07-22 17:07:36 +0700

    update

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,17 @@
 const app = require("../src/index");
 const serverless = require("serverless-http");
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://afanaljafar.github.io/blog-portofolio/",
+    ],
+    credentials: true,
+  })
+);
+app.use(cors());
 
 // module.exports = serverless(app);
 module.exports = (req, res) => {
